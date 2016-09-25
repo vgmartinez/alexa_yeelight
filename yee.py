b72a7ac8193c62a0bd303cd4f60c31e334ff302b
Victor Manuel <viktor.manuel.garcia@gmail.com>
2016-09-25 11:08:38 +0200

    add reconnet for client

--- yee.py
+++ yee.py
@@ -49,7 +49,7 @@ myAWSIoTMQTTClient.configureCredentials("cert/rootCA.pem", "cert/737a0f3c55-priv
 
 # AWSIoTMQTTClient connection configuration
 myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-#myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
